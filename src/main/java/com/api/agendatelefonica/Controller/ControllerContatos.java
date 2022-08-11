eddf7e1fcf6606632f76d1f41d6d8bd3e0ab02db
pauloren7 <paulorenan1515@gmail.com>
2022-08-11 18:39:39 -0300

    aplicando menssagens de validações

--- src/main/java/com/api/agendatelefonica/Controller/ControllerContatos.java
+++ src/main/java/com/api/agendatelefonica/Controller/ControllerContatos.java
@@ -6,12 +6,17 @@ import com.api.agendatelefonica.Service.ServicesContatos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -38,8 +43,16 @@ public class ControllerContatos {
     }
 
     @PutMapping(path = "/agenda/{id}")
-    public ModelContatos alterarContato(@RequestBody ModelContatos modelContatos) {
-        return servicesContatos.alterarContato(modelContatos);
+    public ResponseEntity<Object> alterarContato(@RequestBody @Valid ModelContatos modelContatos) {
+        if (servicesContatos.existsByNumeroTelefone(modelContatos.getNumeroTelefone())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: Número de telefone já está sendo usado!");
+        }
+        if (servicesContatos.existsByEmail(modelContatos.getEmail())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: E-mail já está em uso!");
+        }
+
+        modelContatos.setDataRegistro(LocalDateTime.now(ZoneId.of("UTC-03:00")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(servicesContatos.alterarContato(modelContatos));
     }
 
     @GetMapping(path = "/agenda/{id}")
@@ -50,6 +63,18 @@ public class ControllerContatos {
     @DeleteMapping(path = "/agenda/{id}")
     public void excluirContato(@PathVariable Long id) {
         servicesContatos.excluirContato(id);
+    }
+    //método para aplicar validações feitas na classe model
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> validacaoDeErros(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String campoNome = ((FieldError) error).getField();
+            String mensagemDeErro = error.getDefaultMessage();
+            errors.put(campoNome, mensagemDeErro);
+        });
+        return errors;
     }
 
 }
--- src/main/java/com/api/agendatelefonica/Model/ModelContatos.java
+++ src/main/java/com/api/agendatelefonica/Model/ModelContatos.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
@@ -22,15 +23,16 @@ public class ModelContatos {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = false, length = 50)
-    @NotBlank
+    @NotBlank(message = "Campo não informado")
+    @Pattern(regexp = "^[A-Z]+(.)*", message = "O nome deve iniciar com letra maiúscula")
     private String nome;
     @Column(nullable = false, unique = false, length = 50)
-    @NotBlank
-    @Size(min = 6, max = 14)
+    @NotBlank(message = "Campo não informado")
+    @Size(min = 6, max = 14, message = "O número de telefone precisa ter entre 6 e 14 caracteres")
     private String numeroTelefone;
     @Column(nullable = false, unique = false, length = 50)
-    @NotBlank
-    @Email
+    @NotBlank(message = "Campo não informado")
+    @Email(message = "Informe um e-mail válido")
     private String email;
     @Column(nullable = false, length = 50)
     private LocalDateTime dataRegistro;
