d8576a440566a350ee3dcd1b459238105eb5e4a3
pauloren7 <paulorenan1515@gmail.com>
2022-08-12 17:31:22 -0300

    ajustes no formato de data e hora

--- src/main/java/com/api/agendatelefonica/Model/ModelContatos.java
+++ src/main/java/com/api/agendatelefonica/Model/ModelContatos.java
@@ -22,15 +22,15 @@ public class ModelContatos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = false, length = 50)
+    @Column(nullable = false, unique = true, length = 50)
     @NotBlank(message = "Campo não informado")
     @Pattern(regexp = "^[A-Z]+(.)*", message = "O nome deve iniciar com letra maiúscula")
     private String nome;
-    @Column(nullable = false, unique = false, length = 50)
+    @Column(nullable = false, unique = true, length = 50)
     @NotBlank(message = "Campo não informado")
     @Size(min = 6, max = 14, message = "O número de telefone precisa ter entre 6 e 14 caracteres")
     private String numeroTelefone;
-    @Column(nullable = false, unique = false, length = 50)
+    @Column(nullable = false, unique = true, length = 50)
     @NotBlank(message = "Campo não informado")
     @Email(message = "Informe um e-mail válido")
     private String email;
